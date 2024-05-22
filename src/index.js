d226a62791f247dff79fbaceccc6038ec926c8e6
Arraj2611 <arrajeevaken2611@gmail.com>
2024-05-22 13:11:47 +0530

    h1 in index.js removed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello React</h1>
     <App />
   </React.StrictMode>
 );
