315f0416f3be361e5ac2aaae6a13acd2def36caa
Arraj2611 <arrajeevaken2611@gmail.com>
2024-05-22 13:10:09 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,8 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learning React
-          </a>
-        </header>
+      <div className="f1 tc">
+        <h1>Hello world</h1>
       </div>
     );
   }
