097740dbb9f41c2356b7f5a8470885fca716791a
Arraj2611 <arrajeevaken2611@gmail.com>
2024-05-22 15:04:59 +0530

    edited

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
+import Cardlist from './CardList';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
