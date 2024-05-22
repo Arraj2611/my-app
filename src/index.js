98449a2ecf2a882f7592299208f016970ab13eac
Arraj2611 <arrajeevaken2611@gmail.com>
2024-05-22 13:34:55 +0530

    editing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App greeting={'Hello React Ninja'}/>
+    <Card/>
   </React.StrictMode>
 );
 
